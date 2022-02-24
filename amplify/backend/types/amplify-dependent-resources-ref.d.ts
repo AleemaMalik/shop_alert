7f632819ff8154612935b5e8146cd2844ef946b8
bahareh-farhadi <53280095+bahareh-farhadi@users.noreply.github.com>
2022-02-24 12:20:05 -0500

    idk

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "baharehdev79621402": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "baharehdev": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import './App.css';
 import TopBar from './components/TopBar/TopBar';
 
 
@@ -11,9 +11,33 @@ import {SignUp} from './components/Home/SignUp'
 import {LogIn} from './components/Home/LogIn'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
+import {listSongs} from './graphql/queries'; 
 
 
 function App() {
+  
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() =>{
+    fetchSongs();
+
+  }, []); //without [] this will call fetchSongs in an infinite loop, so adding [] makes it call fetchSongs only once 
+  const fetchSongs = async() => {
+    try{
+      
+      const songData = await API.graphql(graphqlOperation(listSongs)); //get the song database
+      const songList = songData.data.listSongs.items;
+      console.log('song list', songList);
+      setSongs(songList);
+
+    }
+    catch (error){
+      console.log('error on fetching songs', error);
+
+    }
+
+  }
   return (
  
    <BrowserRouter>
