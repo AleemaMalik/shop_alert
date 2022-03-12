484e9740f5437e6f9e1ea6ee4d13b917340034d9
Sadat <sadatarraafe@gmail.com>
2022-03-12 14:22:50 -0500

    HTTP CORS policy fixed

--- src/components/Popups/InsertItemPopup.js
+++ src/components/Popups/InsertItemPopup.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import './InsertItemPopup.css'
+import React from "react";
+import "./InsertItemPopup.css";
+import WebScraper from "../../webscraper/WebScraper";
 
 function InsertItemPopup(props) {
-    return (props.triggerInsertItem) ? (
-        <div className="insert-item-popup">
-            <div className="insert-item-popup-content">
-                <h3>Insert Item URL</h3>
-                <input type="url" placeholder="Insert URL..."/>
-                <button onClick={() => props.setTriggerItemInfo(true)}>Enter</button>
-                <button onClick={() => props.setTriggerInsertItem(false)}>Cancel</button>
-            </div>   
-        </div>
-    ) : "";
+  let scraper = new WebScraper();
+  scraper.getItemsFromSearch("mouse pad", "amazon.ca").then((info) => {
+    console.log(info);
+  });
+
+  return props.triggerInsertItem ? (
+    <div className="insert-item-popup">
+      <div className="insert-item-popup-content">
+        <h3>Insert Item URL</h3>
+        <input type="url" placeholder="Insert URL..." />
+        <button onClick={() => props.setTriggerItemInfo(true)}>Enter</button>
+        <button onClick={() => props.setTriggerInsertItem(false)}>Cancel</button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
 
-export default InsertItemPopup
+export default InsertItemPopup;
