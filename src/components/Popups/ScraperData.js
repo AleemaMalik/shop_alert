f8c01323ade5180b6b8db22c66e0c0c4ea99e26f
Sadat <sadatarraafe@gmail.com>
2022-04-08 19:08:58 -0400

    Implemented live data of adding items via URL and search bar (Amazon only)

--- src/components/Popups/ScraperData.js
+++ src/components/Popups/ScraperData.js
@@ -1,63 +1,62 @@
-export const ScraperData =[
-    {
-        productID: 'B06X3W3YQD',
-        name: 'AmazonBasics Gaming Computer Mouse Pad - Black',
-        price: '$11.69',
-        imageURL: 'https://m.media-amazon.com/images/I/71o2ghrFt3L._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B088P53Y6X',
-        name: 'Gaming Mouse PadAnti-Slip Rubber Base Oversized Non-Slip 31.5x11.8 inches, Laptop Desk mat Desktop, Office and Professional Gaming Mouse pad (black-01)',
-        price: '$17.99',
-        imageURL: 'https://m.media-amazon.com/images/I/61fegGLd7YL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B09S9N13DC',
-        name: 'Mouse Mat Black Anti-Slip Comfort Mouse PAD MAT with Gel Foam Rest Wrist Support for PC Laptop - Compatible with Laser and Optical Mice',
-        price: '$11.99',
-        imageURL: 'https://m.media-amazon.com/images/I/71DshNvpHhL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B08L3V1TW2',
-        name: 'Leather Desk Pad, Large Mouse Pads 80 x 40cm Laptop Desk Mat Nonslip Base and Waterproof Desk Protector for Office Home Computer',
-        price: '$27.98',
-        imageURL: 'https://m.media-amazon.com/images/I/718AhCPf4UL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B088P53Y6X',
-        name: 'Gaming Mouse PadAnti-Slip Rubber Base Oversized Non-Slip 31.5x11.8 inches, Laptop Desk mat Desktop, Office and Professional Gaming Mouse pad (black-01)',
-        price: '$17.99',
-        imageURL: 'https://m.media-amazon.com/images/I/61fegGLd7YL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B088BSV2KV',
-        name: 'Rii Mouse pad,Ergonomic Memory Foam Mouse Pad,Mouse Pad Wrist Rest,Non-Slip Rubber Base Rest Mouse Pad Wrist Rest Support, Soft Mouse Mat for Gaming,Office,Computer,Laptop,Mac,Black',
-        price: '$11.90',
-        imageURL: 'https://m.media-amazon.com/images/I/81Ftq0mOeiL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B000UEZ37G',
-        name: 'SteelSeries QcK Mini Gaming Mouse Pad (Black) (63005)',
-        price: '$10.99',
-        imageURL: 'https://m.media-amazon.com/images/I/51xQekVdo9L._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B086S4YRLD',
-        name: 'ITNRSIIET [20% Larger] Mouse Pad with Stitched Edge Premium-Textured Mouse Mat Waterproof Non-Slip Rubber Base Round Mousepad for Laptop Computer PC Office 8.7×8.7×0.12 inches Modern Marbling',
-        price: '$9.98',
-        imageURL: 'https://m.media-amazon.com/images/I/71S0I16Jb6L._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B085HGV3N8',
-        name: 'ITNRSIIET Mouse Pad, Ergonomic Mouse Pad with Gel Wrist Rest Support, Gaming Mouse Pad with Lycra Cloth, Non-Slip PU Base for Computer, Laptop, PC, Home, Office & Travel, Black',
-        price: '$14.88',
-        imageURL: 'https://m.media-amazon.com/images/I/61Q7az8zwKL._AC_UL320_.jpg'
-      },
-      {
-        productID: 'B06X3W3YQD',
-        name: 'AmazonBasics Gaming Computer Mouse Pad - Black',
-        price: '$11.69',
-        imageURL: 'https://m.media-amazon.com/images/I/71o2ghrFt3L._AC_UL320_.jpg'
-      }
-    ]
-    
+export const defaultScraperData = [
+  {
+    productID: "B06X3W3YQD",
+    name: "AmazonBasics Gaming Computer Mouse Pad - Black",
+    price: "$11.69",
+    imageURL: "https://m.media-amazon.com/images/I/71o2ghrFt3L._AC_UL320_.jpg",
+  },
+  {
+    productID: "B088P53Y6X",
+    name: "Gaming Mouse PadAnti-Slip Rubber Base Oversized Non-Slip 31.5x11.8 inches, Laptop Desk mat Desktop, Office and Professional Gaming Mouse pad (black-01)",
+    price: "$17.99",
+    imageURL: "https://m.media-amazon.com/images/I/61fegGLd7YL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B09S9N13DC",
+    name: "Mouse Mat Black Anti-Slip Comfort Mouse PAD MAT with Gel Foam Rest Wrist Support for PC Laptop - Compatible with Laser and Optical Mice",
+    price: "$11.99",
+    imageURL: "https://m.media-amazon.com/images/I/71DshNvpHhL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B08L3V1TW2",
+    name: "Leather Desk Pad, Large Mouse Pads 80 x 40cm Laptop Desk Mat Nonslip Base and Waterproof Desk Protector for Office Home Computer",
+    price: "$27.98",
+    imageURL: "https://m.media-amazon.com/images/I/718AhCPf4UL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B088P53Y6X",
+    name: "Gaming Mouse PadAnti-Slip Rubber Base Oversized Non-Slip 31.5x11.8 inches, Laptop Desk mat Desktop, Office and Professional Gaming Mouse pad (black-01)",
+    price: "$17.99",
+    imageURL: "https://m.media-amazon.com/images/I/61fegGLd7YL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B088BSV2KV",
+    name: "Rii Mouse pad,Ergonomic Memory Foam Mouse Pad,Mouse Pad Wrist Rest,Non-Slip Rubber Base Rest Mouse Pad Wrist Rest Support, Soft Mouse Mat for Gaming,Office,Computer,Laptop,Mac,Black",
+    price: "$11.90",
+    imageURL: "https://m.media-amazon.com/images/I/81Ftq0mOeiL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B000UEZ37G",
+    name: "SteelSeries QcK Mini Gaming Mouse Pad (Black) (63005)",
+    price: "$10.99",
+    imageURL: "https://m.media-amazon.com/images/I/51xQekVdo9L._AC_UL320_.jpg",
+  },
+  {
+    productID: "B086S4YRLD",
+    name: "ITNRSIIET [20% Larger] Mouse Pad with Stitched Edge Premium-Textured Mouse Mat Waterproof Non-Slip Rubber Base Round Mousepad for Laptop Computer PC Office 8.7×8.7×0.12 inches Modern Marbling",
+    price: "$9.98",
+    imageURL: "https://m.media-amazon.com/images/I/71S0I16Jb6L._AC_UL320_.jpg",
+  },
+  {
+    productID: "B085HGV3N8",
+    name: "ITNRSIIET Mouse Pad, Ergonomic Mouse Pad with Gel Wrist Rest Support, Gaming Mouse Pad with Lycra Cloth, Non-Slip PU Base for Computer, Laptop, PC, Home, Office & Travel, Black",
+    price: "$14.88",
+    imageURL: "https://m.media-amazon.com/images/I/61Q7az8zwKL._AC_UL320_.jpg",
+  },
+  {
+    productID: "B06X3W3YQD",
+    name: "AmazonBasics Gaming Computer Mouse Pad - Black",
+    price: "$11.69",
+    imageURL: "https://m.media-amazon.com/images/I/71o2ghrFt3L._AC_UL320_.jpg",
+  },
+];
